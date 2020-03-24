52ac7eef75d9987d79ef3e18d2397fc549f8c1cd
KamilStrus <50923833+KamilStrus@users.noreply.github.com>
2020-03-24 19:50:13 +0100

    Dodaj pliki projektów.

---
+++ Onion.Domain/Entities/Student.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Onion.Domain.Entities
+{
+    public class Student
+    {
+        public Student( /*string nid*/ string firstName, string lastName)
+        {
+            //  ID = nid;
+
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Onion.Infrastructure.SqlDbService/Services/SqlDbService.cs
@@ -0,0 +1,62 @@
+﻿using Onion.Domain.Entities;
+using Onion.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Onion.Infrastructure.SqlDbService.Services
+{
+    public class SqlDbService : IStudentDbService
+    {
+        private static readonly ICollection<Student> _students = new List<Student>();
+        public IEnumerable<Student> GetStudents()
+        {
+            
+            var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s17090;Integrated Security=True");
+            var com = new SqlCommand();
+
+            com.Connection = con;
+            com.CommandText = "select * from Students";
+            
+            con.Open();
+            SqlDataReader dr = com.ExecuteReader();
+            int i = 0;
+            while (dr.Read())
+            {
+                string numerid = dr["IdStudent"].ToString();
+                _students.Add(new Student
+                (
+                 
+
+                  dr["FirstName"].ToString(),
+                  dr["LastName"].ToString()
+
+
+             ));
+                _students.ElementAt(i).IdStudent = int.Parse(numerid);
+                i++;
+
+            }
+
+
+            con.Dispose();
+            return _students;
+        }
+        public bool EnrollStudent(Student newStudent, int semestr)
+        {
+
+            var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s17090;Integrated Security=True");
+            var com = new SqlCommand();
+
+            com.Connection = con;
+            con.Open();
+            com.CommandText = "INSERT INTO Students (FirstName, LastName) VALUES('" + newStudent.FirstName + "','" + newStudent.LastName + "')";
+            con.Dispose();
+
+            return true;
+        }
+    }
+}
